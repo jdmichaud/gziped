fc131ac196e2899e991b6a861851e8d7952b90c5
jd <jean.daniel.michaud@gmail.com>
2018-08-12 22:36:26 +0200

    Add basic signature

--- src/js/gziped.ts
+++ src/js/gziped.ts
@@ -1,2 +1,11 @@
+class Position {
+  public bufferIndex = 0;
+  public mask = 0;
+}
+
+function inflate_block(buf: Uint8array, pos: Position,
+                       litdict: any, distdict: any, buf: IndexedBuffer<Uint8array>): void {
+}
+
 export function inflate(buf: Uint8Array, output: Uint8Array): void {
 }
